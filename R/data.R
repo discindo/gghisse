2a489f06823b8b7f259b00e84c5bf3ec46a0c170
teofiln <teofiln@gmail.com>
2019-05-29 20:41:26 -0500

    documentation for the new data object

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Some hisse object from a diatom dataset
+#'
+#'
+#' @format A list with 5 elements
+#' \describe{
+#'   \item{cid4_recon}{hisse.states object. A marginal ancestral state reconstruction of diatom habitat (plankton-benthos) under hisse's CID4 model}
+#'   \item{cid8}{muhisse.fit object. A CID8 model fit object for data on diatom salinity (marine-freshwater) and habitat (plankton-benthos)}
+#'   \item{muhisse}{muhisse.fit object. A MuHiSSE model fit object for data on diatom salinity (marine-freshwater) and habitat (plankton-benthos)}
+#'   \item{muhisse_recon}{muhisse.states object. A marginal ancestral state reconstruction of diatom salinity (marine-freshwater) and habitat (plankton-benthos) under hisse's MuHiSSE model}
+#'   \item{musse}{muhisse.fit object. A MuSSE model fit object for data on diatom salinity (marine-freshwater) and habitat (plankton-benthos)}
+#' }
+#' @references \url{https://www.biorxiv.org/content/10.1101/406165v2.abstract}
+"diatoms"
